417dd8bb873916a071714abd2a4197a54f3a22a2
Pham Dinh Hieu <dinhieufam@Phams-MacBook-Air.local>
2025-05-14 21:27:35 +0700

    Spring Initializer

---
+++ backend/src/test/java/dev/vintrigue/gymmate/GymmateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vintrigue.gymmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
