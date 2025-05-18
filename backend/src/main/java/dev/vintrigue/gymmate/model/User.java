a892a79a838183f7f3bb4fae9d309f7b226592a6
NguyenCungThanh <thanhnguyen@Thanhs-Macbook-Air-M2.local>
2025-05-19 02:30:17 +0700

    Newest version

--- backend/src/main/java/dev/vintrigue/gymmate/model/User.java
+++ backend/src/main/java/dev/vintrigue/gymmate/model/User.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Document(collection = "users")
+@Document(collection = "user")
 @Data
 public class User {
     @Id
