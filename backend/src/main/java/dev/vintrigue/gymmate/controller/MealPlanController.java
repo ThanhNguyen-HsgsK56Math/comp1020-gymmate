18260426c58b6eb8f583eb7185277b92c1491e34
minhdang-DS <minhdang050806@gmail.com>
2025-05-23 15:30:02 +0000

    backend 10:29pm 23/5

--- backend/src/main/java/dev/vintrigue/gymmate/controller/MealPlanController.java
+++ backend/src/main/java/dev/vintrigue/gymmate/controller/MealPlanController.java
@@ -1,10 +1,13 @@
 package dev.vintrigue.gymmate.controller;
 
+import dev.vintrigue.gymmate.model.Meal;
 import dev.vintrigue.gymmate.model.MealPlan;
 import dev.vintrigue.gymmate.service.MealPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/mealplans")
@@ -12,6 +15,18 @@ public class MealPlanController {
 
     @Autowired
     private MealPlanService mealPlanService;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    /**
+     * Test endpoint to check available meals
+     */
+    @GetMapping("/test-meals")
+    public ResponseEntity<?> testMeals() {
+        List<Meal> meals = mongoTemplate.findAll(Meal.class);
+        return ResponseEntity.ok(meals);
+    }
 
     /**
      * Generates a meal plan for a user based on their profile and goals
--- backend/src/main/java/dev/vintrigue/gymmate/model/Meal.java
+++ backend/src/main/java/dev/vintrigue/gymmate/model/Meal.java
@@ -3,17 +3,58 @@ package dev.vintrigue.gymmate.model;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 @Document(collection = "meal")
 @Data
 public class Meal {
     @Id
     private String id;
+
     private String name;
     private String description;
-    private int calories; // Calorie intake
-    private int prepTime; // in minutes
-    private Map<String, Integer> parameters; // e.g., {"weight_loss": 8, "muscle_gain": 7, "general_health": 8}
+
+    private String type;
+
+    @Field("suitability_weight_loss")
+    private int suitabilityWeightLoss;
+
+    @Field("suitability_muscle_gain")
+    private int suitabilityMuscleGain;
+
+    @Field("suitability_endurance_building")
+    private int suitabilityEnduranceBuilding;
+
+    @Field("suitability_healthy_lifestyle")
+    private int suitabilityHealthyLifestyle;
+
+    private int calories;
+
+    @Field("follow-up_meals")
+    private List<String> followUpMeals;
+
+    // Default prep time in minutes if not specified
+    private int prepTime = 15;
+
+    private Map<String, Integer> parameters;
+
+    // Computed property for algorithm use
+    public Map<String, Integer> getParameters() {
+        if (parameters == null) {
+            parameters = new HashMap<>();
+            parameters.put("suitability_weight_loss", suitabilityWeightLoss);
+            parameters.put("suitability_muscle_gain", suitabilityMuscleGain);
+            parameters.put("suitability_endurance_building", suitabilityEnduranceBuilding);
+            parameters.put("suitability_healthy_lifestyle", suitabilityHealthyLifestyle);
+        }
+        return parameters;
+    }
+
+    public void setParameters(Map<String, Integer> parameters) {
+        this.parameters = parameters;
+    }
 }
